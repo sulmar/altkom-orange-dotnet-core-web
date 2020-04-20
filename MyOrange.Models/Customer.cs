7fccceeec59d376f7c2c1b3d471760d4e726ba97
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-04-20 09:15:21 +0000

    Init

---
+++ MyOrange.Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyOrange.Models
+{
+    public abstract class Base
+    {
+        public int Id { get; set; }
+    }
+
+    public class Customer : Base
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool IsRemoved { get; set; }
+    }
+}
