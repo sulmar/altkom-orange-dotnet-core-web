a8d29c6c9f52ff9b8afc10a7d6470e8c2dcd89b5
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-04-20 10:09:38 +0000

    CustomerFaker

---
+++ MyOrange.FakeServices/Fakers/CustomerFaker.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using MyOrange.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyOrange.FakeServices.Fakers
+{
+    public class CustomerFaker : Faker<Customer>
+    {
+        // snippet: ctor + 2 x tab
+        public CustomerFaker()
+        {
+            RuleFor(p => p.Id, f => f.IndexFaker + 1);
+            RuleFor(p => p.FirstName, f => f.Person.FirstName);
+            RuleFor(p => p.LastName, f => f.Person.LastName);
+            RuleFor(p => p.Email, f => f.Person.Email);
+            RuleFor(p => p.IsRemoved, f => f.Random.Bool(0.3f));
+        }
+    }
+}
