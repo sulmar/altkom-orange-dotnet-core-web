eb50bedc6df19bed8cec508f756e97b5bb820431
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-04-23 14:02:42 +0000

    ViewComponent

---
+++ MyOrange.Web/ViewComponents/CustomerCardViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyOrange.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyOrange.Web.ViewComponents
+{
+    public class CustomerCardViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Customer customer)
+        {
+            return View(customer);
+        }
+    }
+}
