de9979cd5852c08c0f7759bc4df7f739cc717dc7
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-04-23 12:11:12 +0000

    refaktoring

---
+++ MyOrange.Web/DbHostExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MyOrange.DbServices;
+
+namespace MyOrange.Web
+{
+    public static class DbHostExtensions
+    {
+        public static IHost CreateDbIfNotExists<TContext>(this IHost host)
+            where TContext : DbContext
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<MyOrangeContext>();
+
+                try
+                {
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Create database error");
+                }
+
+            }
+
+            return host;
+        }
+
+
+        public static IHost MigrateDb<TContext>(this IHost host)
+        where TContext : DbContext
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<TContext>();
+
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Apply migration has failed");
+                }
+
+            }
+
+            return host;
+        }
+
+    }
+}
--- MyOrange.Web/Program.cs
+++ MyOrange.Web/Program.cs
@@ -1,13 +1,9 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using MyOrange.DbServices;
 
 namespace MyOrange.Web
@@ -25,58 +21,5 @@ namespace MyOrange.Web
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-    }
-
-    public static class DbHostExtensions
-    {
-        public static IHost CreateDbIfNotExists<TContext>(this IHost host)
-            where TContext : DbContext
-        {
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                var context = services.GetRequiredService<MyOrangeContext>();
-
-                try
-                {
-                    context.Database.EnsureCreated();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "Create database error");
-                }
-
-            }
-
-            return host;
-        }
-
-
-        public static IHost MigrateDb<TContext>(this IHost host)
-        where TContext : DbContext
-        {
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                try
-                {
-                    var context = services.GetRequiredService<MyOrangeContext>();
-
-                    context.Database.Migrate();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "Apply migration has failed");
-                }
-
-            }
-
-            return host;
-        }
-
     }
 }
