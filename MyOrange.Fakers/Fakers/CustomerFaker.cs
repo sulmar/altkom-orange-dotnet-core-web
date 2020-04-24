dd80831ce8b18280ec83db20484265306efa0ea5
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-04-24 07:04:21 +0000

    Faker

--- MyOrange.Fakers/Fakers/CustomerFaker.cs
+++ MyOrange.Fakers/Fakers/CustomerFaker.cs
@@ -15,11 +15,15 @@ namespace MyOrange.Fakers
             RuleFor(p => p.Id, f => f.IndexFaker + 1);
             RuleFor(p => p.FirstName, f => f.Person.FirstName);
             RuleFor(p => p.LastName, f => f.Person.LastName);
-            RuleFor(p => p.Email, f => f.Person.Email);
+            
+            // RuleFor(p => p.Email, f => f.Person.Email);
+            
+            // john.smith@orange.pl
+            RuleFor(p => p.Email, (f, c) => $"{c.FirstName}.{c.LastName}@orange.pl");
+
             RuleFor(p => p.Country, f => f.Address.Country());
             RuleFor(p => p.Photo, f => f.Person.Avatar);
-            RuleFor(p => p.IsRemoved, f => f.Random.Bool(0.3f));
-
+            RuleFor(p => p.IsRemoved, f => f.Random.Bool(0.3f));           
         }
     }
 }
