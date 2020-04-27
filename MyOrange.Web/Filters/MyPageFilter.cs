a6e2d296501141251fbf1a62e38c8a940b5ebbf7
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-04-27 17:21:16 +0200

    Dodanie filtru

---
+++ MyOrange.Web/Filters/MyPageFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyOrange.Web.Filters
+{
+    public class MyPageFilter : IPageFilter
+    {
+        private readonly IWebHostEnvironment env;
+
+        public MyPageFilter(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        public void OnPageHandlerExecuted(PageHandlerExecutedContext context)
+        {
+        }
+
+        public void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        {
+        }
+
+        public void OnPageHandlerSelected(PageHandlerSelectedContext context)
+        {
+            if (context.HandlerMethod.HttpMethod == "Get" && context.HandlerMethod.MethodInfo.Name == "OnGet")
+            {
+                // code placed here will only execute if the OnGet() method has been selected
+
+                context.HttpContext.Response.Headers.Add("environment", env.EnvironmentName);
+            }
+        }
+    }
+}
